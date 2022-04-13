b5a834b58f899c79f0245be48f7f23476b5fafee
ellesent <hypahflipperz@gmail.com>
2022-04-13 12:09:06 -0700

    stack fully implemented and tests passing

--- src/data-structures/stack/Stack.js
+++ src/data-structures/stack/Stack.js
@@ -8,60 +8,29 @@ export default class Stack {
     this.linkedList = new LinkedList();
   }
 
-  /**
-   * @return {boolean}
-   */
   isEmpty() {
-    // The stack is empty if its linked list doesn't have a head.
-    return !this.linkedList.head;
+    return this.linkedList.head === null;
   }
 
-  /**
-   * @return {*}
-   */
-  peek() {
-    if (this.isEmpty()) {
-      // If the linked list is empty then there is nothing to peek from.
-      return null;
-    }
-
-    // Just read the value from the start of linked list without deleting it.
-    return this.linkedList.head.value;
+  toString(callback) {
+    return this.linkedList.toString(callback);
   }
 
-  /**
-   * @param {*} value
-   */
   push(value) {
-    // Pushing means to lay the value on top of the stack. Therefore let's just add
-    // the new value at the start of the linked list.
     this.linkedList.prepend(value);
   }
 
-  /**
-   * @return {*}
-   */
+  peek() {
+    return this.linkedList.head ? this.linkedList.head.value : null;
+  }
+
   pop() {
-    // Let's try to delete the first node (the head) from the linked list.
-    // If there is no head (the linked list is empty) just return null.
-    const removedHead = this.linkedList.deleteHead();
-    return removedHead ? removedHead.value : null;
+    const node = this.linkedList.deleteHead();
+
+    return node ? node.value : null;
   }
 
-  /**
-   * @return {*[]}
-   */
   toArray() {
-    return this.linkedList
-      .toArray()
-      .map((linkedListNode) => linkedListNode.value);
-  }
-
-  /**
-   * @param {function} [callback]
-   * @return {string}
-   */
-  toString(callback) {
-    return this.linkedList.toString(callback);
+    return this.linkedList.toArray().map((node) => node.value);
   }
 }
